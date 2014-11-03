0b32340a73f16d1ac9dde8ab98acbf815b63bb77
johnthekid <rivera512jr@gmail.com>
2014-11-03 16:00:43 -0600

    Lab 11

--- lab11-70pt.py
+++ lab11-70pt.py
@@ -4,32 +4,27 @@
 #                                        #
 ##########################################
 
-# Make the ball twice as large
-# Make the ball move 5 times faster
-
 from Tkinter import *
 root = Tk()
-# Create the canvas widget
+
 drawpad = Canvas(root, width=800,height=600, background='white')
 drawpad.grid(row=0, column=0)
 
-# Create the oval
-circle = drawpad.create_oval(10, 10, 50, 50, fill='green')
+circle = drawpad.create_oval(10, 10, 100, 100, fill='green')
 direction = 1
-# Create our animation function
+
 def animate():
     global direction
-    # Get the x and y co-ordinates of the circle
+  
     x1, y1, x2, y2 = drawpad.coords(circle)
-    if x2 > drawpad.winfo_width(): 
-        direction = - 1
-    elif x1 < 0:
-        direction = 1
-    #Move our oval object by the value of direction
-    drawpad.move(circle,direction,0)
-    # Wait for 1 millisecond, then recursively call our animate function
+    if y2 > drawpad.winfo_height(): 
+        direction = - 5
+    elif y1 < 0:
+        direction = 5
+
+    drawpad.move(circle,0,direction)
+
     drawpad.after(1, animate)
 
-# Kick off our animation
 animate()
 root.mainloop()
